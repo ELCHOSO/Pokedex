bbad9feb937fe52842574fb947ee53c1518d73f1
Julien Huang <julien.huang@epitech.eu>
2022-01-24 01:11:51 +0100

    [ADD] front + read.md + enableCORS in server

---
+++ READ.md
@@ -0,0 +1,13 @@
+# Pokedex
+
+ReactJS and NestJS app to find a pokemon
+
+## Before starting the App
+
+Install [Docker](https://www.docker.com/get-started)
+
+## Start the App
+
+```bash
+docker-compose up
+```
--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo-pokemon.png'
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+import './App.css'
+import React from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pokemon: {
+        name: '',
+        imageUri: '',
+        description: ''
+      },
+      value: ''
+    }
+    this.searchPokemonOnChange = this.searchPokemonOnChange.bind(this)
+  }
+
+  searchPokemonOnChange(event) {
+    this.setState({
+      value: event.target.value
+    }, () => {
+      axios.get('http://localhost:8000/pokemon/' + this.state.value).then(res => {
+        this.setState({
+          pokemon: res.data
+        })
+      }).catch(e => {
+        this.setState({
+          pokemon: {
+            name: '',
+            imageUri: '',
+            description: "I don't know this pokemon"
+          }
+        })
+      })
+    })
+
+  }
+  render() {
+    return (
+      <div className='app' style={{ backgroundColor: '#003a70', height: '100vh', textAlign: 'center' }} >
+        <img src={logo} alt='logo pokemon' width='20%' height='15%' />
+        <Card className="pokedex">
+          <Card.Body>
+            <Card.Header>Pokedex</Card.Header>
+            <Form.Label>Pokemon ID</Form.Label>
+            <Form.Control placeholder='1, 2, 3...' value={this.state.value} onChange={this.searchPokemonOnChange} />
+            <img src={this.state.pokemon.imageUri} />
+            {this.state.pokemon.name.charAt(0).toUpperCase() + this.state.pokemon.name.slice(1)}<br />
+            {this.state.pokemon.description}
+          </Card.Body>
+        </Card>
+      </div >
+    )
+  }
 }
 
 export default App;
